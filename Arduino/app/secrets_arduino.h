46c4497d0dd7ad46dbf8edea52e9e22ee1227ed9
Mathis-CHALANDARD <101826600+Mathis-CHALANDARD@users.noreply.github.com>
2023-04-26 11:10:55 -0400

    Delete secrets_arduino.h

--- Arduino/app/secrets_arduino.h
+++
@@ -1,8 +0,0 @@
-#define MQTT_BRKR	  "broker.hivemq.com"
-#define MQTT_PORT	  1883
-#define MQTT_RECV   "hydroponey/android/push"
-#define MQTT_SEND   "hydroponey/arduino/push"
-#define MQTT_USER	  "hydroponey"
-#define MQTT_PSWD	  "motdepassesupersecure"
-#define WiFi_SSID	  "COGECO-4C2BF0"
-#define WiFi_PSWD	  "ymzmzw4y"
